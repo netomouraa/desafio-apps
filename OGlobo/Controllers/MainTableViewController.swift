50a3b92df348dd85a1ec78d7f56e8e00c496ddb5
Neto Moura <netomoura@Netos-MacBook-Air.local>
2017-09-29 06:11:53 -0300

    test version

--- OGlobo/Controllers/MainTableViewController.swift
+++ OGlobo/Controllers/MainTableViewController.swift
@@ -12,6 +12,7 @@ import AlamofireImage
 
 class MainTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var mainTableView: UITableView!
 
     var arrayNoticias = [Conteudos]()
@@ -20,6 +21,11 @@ class MainTableViewController: UIViewController, UITableViewDelegate, UITableVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
 
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
 
@@ -31,85 +37,44 @@ class MainTableViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     
     func requestAlamofire() {
-        
-//        self.activityIndicator.startAnimating()
-//        UIApplication.shared.beginIgnoringInteractionEvents()
+        self.activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
         
         Alamofire.request(RouterOGlobo.desafioApps)
             .validate().responseArray { (response: DataResponse<[News]>) in
                 switch response.result {
                 case .success(let data):
-                    print("DATA: \(data)")
-                    
                     for itens in data{
                         for noticia in itens.conteudos!{
-                            
-                            print("NOTICIA: \(noticia.titulo)")
-
                             if noticia.tipo != "linkExterno" {
                                 self.arrayNoticias.append(noticia)
                             }
                         }
                     }
-                    
                     DispatchQueue.main.async() {
                         self.mainTableView.reloadData()
                     }
-
-//                                for item in noticia.imagens!{
-                            
-                                    
-                                    
-//                                    DispatchQueue.main.async() {
-//                                        print("FOTO: \(item.url!)\n")
-//                                        //                                        self.urlImagem.append(item.url!)
-//                                        //                                        print("url: \(self.urlImagem.count)")
-//                                        self.requestImagem(url: item.url!)
-//                                    }
-//                                }
-//                            }
-//                                else {
-//                                print("FOTOVAZIA")
-//                                self.imagemNoticia.append(UIImage(named: "default.png")!)
-//                            }
-                    
-
-//                    self.activityIndicator.stopAnimating()
-//                    UIApplication.shared.endIgnoringInteractionEvents()
-                    
-                    
+                self.activityIndicator.stopAnimating()
+                UIApplication.shared.endIgnoringInteractionEvents()
+                
                 case .failure(let error):
                     print("ERRO! \(error)")
-                    
-//                    self.activityIndicator.stopAnimating()
-//                    UIApplication.shared.endIgnoringInteractionEvents()
+                    self.activityIndicator.stopAnimating()
+                    UIApplication.shared.endIgnoringInteractionEvents()
                 }
         }
-        
     }
     
     func requestImagem( url: String) {
         Alamofire.request(url).responseImage { response in
-            switch response.result {
-            case .success(let data):
-
-//            debugPrint(response)
-//            
-//            print(response.request)
-//            print(response.response)
-//            debugPrint(response.result)
-            
-                print("image downloaded: \(data)")
-                self.imagemNoticia = data
-
-            case .failure(let error):
-
-                print(error)
-
+            if let image = response.result.value {
+                
+                self.imagemNoticia = image
             }
         }
     }
-
+    
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
         if indexPath.section == 0 {
@@ -137,64 +102,60 @@ class MainTableViewController: UIViewController, UITableViewDelegate, UITableVie
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+        var noticias = self.arrayNoticias[indexPath.row]
+
         if indexPath.section == 0 {
-
-            let cell1 = tableView.dequeueReusableCell(withIdentifier: "mainCell1", for: indexPath) as! MainTableViewCell1
-            
-            var noticias = self.arrayNoticias.first!
-
+            noticias = self.arrayNoticias.first!
             for item in noticias.imagens! {
-                requestImagem(url: item.url!)
+                self.requestImagem(url: item.url!)
             }
             
+            let cell1 = tableView.dequeueReusableCell(withIdentifier: "mainCell1", for: indexPath) as! MainTableViewCell1
             cell1.imageViewNoticia1.image = self.imagemNoticia
             cell1.labelNomeSecao1.text = noticias.secao?.nome?.uppercased()
             cell1.labelTituloNoticia1.text = noticias.titulo
-
             return cell1
         } else {
-            let cell2 = tableView.dequeueReusableCell(withIdentifier: "mainCell2", for: indexPath) as! MainTableViewCell2
-            
-            var noticias = self.arrayNoticias[indexPath.row + 1]
-    
             for item in noticias.imagens! {
-                requestImagem(url: item.url!)
+                self.requestImagem(url: item.url!)
             }
 
+            let cell2 = tableView.dequeueReusableCell(withIdentifier: "mainCell2", for: indexPath) as! MainTableViewCell2
             cell2.imageViewNoticia2.image = self.imagemNoticia
             cell2.labelNomeSecao2.text = noticias.secao?.nome?.uppercased()
             cell2.labelTituloNoticia2.text = noticias.titulo
-            
             return cell2
         }
-        
-//        return UITableViewCell()
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.mainTableView.deselectRow(at: indexPath, animated: true)
         
-        let noticias = self.arrayNoticias[indexPath.row]
+        var noticias = self.arrayNoticias[indexPath.row]
+        let imagem = self.imagemNoticia
+
         var autor = ""
-        
         
         if noticias.autores != nil {
             autor = noticias.autores!.first!
         }
+
+        if indexPath.section == 0 {
+            noticias = self.arrayNoticias.first!
+
+        } else {
         
+        }
+
         let DetailsVC: DetailsViewController = self.storyboard?.instantiateViewController(withIdentifier: "detailsVC") as! DetailsViewController
-        
-        DetailsVC.image = self.imagemNoticia
+        DetailsVC.image = imagem
         DetailsVC.titulo = noticias.titulo
         DetailsVC.subTitulo = noticias.subTitulo
         DetailsVC.autor = autor
         DetailsVC.dataHora = noticias.publicadoEm
         DetailsVC.texto = noticias.texto
         DetailsVC.tituloView = noticias.secao?.nome
-        
         self.navigationController?.pushViewController(DetailsVC, animated: true)
     }
-
-
 
 }
